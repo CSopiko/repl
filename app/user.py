286b3662f4fcd6feb32f38c1697d9f169d615bca
CSopiko <skurd18@freeuni.edu.ge>
2021-12-26 06:58:44 +0400

    Update user

---
+++ app/user.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+from typing import Protocol
+
+
+@dataclass
+class User:
+    username: str
+
+    def __str__(self) -> str:
+        return self.username
+
+
+class ChannelObserver(Protocol):
+    user: User
+
+    def on_video_published(self) -> None:
+        pass
+
+
+@dataclass
+class Printer:
+    user: User
+
+    def on_video_published(self) -> None:
+        print(self.user.username)
+
+    def __hash__(self) -> int:
+        return (str(self.user) + "_" + "Printer").__hash__()
+
+
+@dataclass
+class EmailSender:
+    user: User
+
+    def on_video_published(self) -> None:
+        pass
